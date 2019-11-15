53f0bf2a840c4f9300f053a481cda301a83cda2d
Ross Martin <ross@axiomalaska.com>
2019-11-14 17:17:04 -0900

    added babel plugins for TS 3.7 features

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,22 @@
+/* eslint-disable */
+
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { useBabelRc, override } = require('customize-cra');
 
 // via https://medium.com/@tommedema/hot-module-reloading-with-create-react-app-v3-f2c7afe1dae8
 
-module.exports = function override(config, env) {
-    config = rewireReactHotLoader(config, env);
+function setHotLoader(config, env) {
+  config = rewireReactHotLoader(config, env);
 
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        'react-dom': '@hot-loader/react-dom'
-    }
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'react-dom': '@hot-loader/react-dom'
+  };
 
   return config;
 }
+
+module.exports = override(
+  useBabelRc(),
+  setHotLoader
+);
